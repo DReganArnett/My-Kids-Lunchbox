189f00b99d31a3a17481087b75c6fcbf8201694f
Danielle Regan Arnett <126263892+DReganArnett@users.noreply.github.com>
2023-09-22 10:41:05 -0600

    Added build folder

---
+++ frontend/build/precache-manifest.18d847d940965043ad2a0aac16c482e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "402fccabd00d103c3360",
+    "url": "/static/css/main.1df53f0e.chunk.css"
+  },
+  {
+    "revision": "402fccabd00d103c3360",
+    "url": "/static/js/main.402fccab.chunk.js"
+  },
+  {
+    "revision": "4cc381ce64c9ce5a9c18",
+    "url": "/static/js/1.4cc381ce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d6f43baaf54eb20f07f0c42d4c73d479",
+    "url": "/index.html"
+  }
+];
